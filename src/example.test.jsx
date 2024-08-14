ee4b01bcf8acf82ba2b9aee0df7fb2f18d6eda17
Stefan Krüger <git@s-light.eu>
2024-08-14 14:55:14 +0000

    update

--- src/example.test.jsx
+++ src/example.test.jsx
@@ -1,20 +1,33 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import MyExample from ".";
+import MyExample from "./example";
 
 // based on https://stackoverflow.com/questions/44686077/how-to-use-jest-to-test-file-download
-import FileSaver from "file-saver";
-vi.mock("file-saver", () => {
+// import FileSaver from "file-saver";
+// vi.mock("file-saver", () => {
+//   return {
+//     default: { saveAs: vi.fn() },
+//     saveAs: vi.fn(),
+//   };
+// });
+
+// global.Blob = function (content, options) {
+//   return { content, options };
+// };
+
+import { saveAs } from "file-saver"; // import file saver
+//Mock file-saver
+vi.mock("file-saver", async (importOriginal) => {
+  const mod = await importOriginal();
   return {
-    default: { saveAs: vi.fn() },
+    ...mod,
     saveAs: vi.fn(),
   };
 });
 
-global.Blob = function (content, options) {
-  return { content, options };
-};
+// Mock URL.createObjectURL using vi.fn() and add it in your beforeach
+global.URL.createObjectURL = vi.fn(() => "blob:mock-url");
 
 describe("MyExample", () => {
   test("check template download", async () => {
@@ -26,8 +39,8 @@ describe("MyExample", () => {
     expect(btn).toBeInTheDocument();
     await user.click(btn);
 
-    expect(FileSaver.saveAs).toHaveBeenCalled();
-    expect(FileSaver.saveAs).toHaveBeenCalledWith(
+    expect(saveAs).toHaveBeenCalled();
+    expect(saveAs).toHaveBeenCalledWith(
       {
         size: 5531,
         options: {
